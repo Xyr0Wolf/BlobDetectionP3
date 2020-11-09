f87e912d025df2b8febe1abbcb145c07aecaf56b
MrFairyDragon <60883151+MrFairyDragon@users.noreply.github.com>
2020-11-09 15:07:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,117 @@
-import cv2
 import numpy as np
+import cv2 as cv
+from Opticalflow import Opticalflow
 
-# open or read the images
-img1 = cv2.imread('cat 1.PNG')
-img2 = cv2.imread('cat 2.PNG')
 
-# resize the images to speed up processing
-img1 = cv2.resize(img1,(640,480))
-img2 = cv2.resize(img2,(640,480))
+class Camshift:
 
-# display resized images
-cv2.imshow("Image 1", img1)
-cv2.imshow("Image 2", img2)
+    def __init__(self, video):
+        # Read the input video
+        cap = cv.VideoCapture(video)
+        cap.set(1, 270)
 
-# convert images to grayscale. This reduces matrices from 3 (R, G, B) to just 1
-gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        cv.imshow('frame', cap.read()[1])
+        print(cap.read()[1].shape)
+        # take first frame of the
+        # video
+        ret, frame = cap.read()
 
-# display grayscale images
-cv2.imshow("Gray 1", gray1)
-cv2.imshow("Gray 2", gray2)
+        # setup initial region of
+        # tracker
+        x, y, width, height = 200, 60, 200, 200
+        track_window = (x, y,
+                        width, height)
 
-# blue the images to get rid of sharp edges/outlines. This will improve the processing
-gray1 = cv2.GaussianBlur(gray1, (21, 21), 0)
-gray2 = cv2.GaussianBlur(gray2, (21, 21), 0)
+        # set up the Region of
+        # Interest for tracking
+        roi = frame[y:y + height,
+              x: x + width]
+        # convert ROI from BGR to
+        # HSV format
+        hsv_roi = cv.cvtColor(roi,
+                              cv.COLOR_BGR2HSV)
 
-# display blurred images
-cv2.imshow("Blur 1", gray1)
-cv2.imshow("Blur 2", gray2)
+        roi_hist = cv.calcHist([hsv_roi],
+                               [0], None,
+                               [180],
+                               [0, 180])
 
-# obtain the difference between the two images & display the result
-imgDelta = cv2.absdiff(gray1, gray2)
-cv2.imshow("Delta", imgDelta)
+        cv.normalize(roi_hist, roi_hist,
+                     0, 255, cv.NORM_MINMAX)
 
-# convert the difference into binary & display the result
-thresh = cv2.threshold(imgDelta, 25, 255, cv2.THRESH_BINARY)[1]
-cv2.imshow("Thresh", thresh)
+        # Setup the termination criteria,
+        # either 15 iteration or move by
+        # atleast 2 pt
+        term_crit = (cv.TERM_CRITERIA_EPS |
+                     cv.TERM_CRITERIA_COUNT, 10, 1)
 
-# dilate the threshhold image to fill in holes & display the result
-thresh = cv2.dilate(thresh, None, iterations=2)
-cv2.imshow("Dilate", thresh)
+        while True:
 
-# find contours or continuous white blobs in the image
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            ret, frame = cap.read()
 
-# find the index of the largest contour - STRING WITCHRAFT POWAAAAAAAA
-areas = [cv2.contourArea(c) for c in contours]
-max_index = np.argmax(areas)
-cnt=contours[max_index]
+            #Resize the video frames.
+            frame = cv.resize(frame,
+                              (720, 720),
+                              fx=0, fy=0,
+                              interpolation=cv.INTER_CUBIC)
 
-# draw a bounding box/rectangle around the largest contour
-x,y,w,h = cv2.boundingRect(cnt)
-cv2.rectangle(img2,(x,y),(x+w,y+h),(0,255,0),2)
 
-# display the original image for reference
-cv2.imshow("Show",img2)
 
-#wait for key press and then terminate all open windows
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+            cv.imshow('Original', frame)
+
+            # perform thresholding on
+            # the video frames
+            ret1, frame1 = cv.threshold(frame,
+                                        180, 155,
+                                        cv.THRESH_TOZERO_INV)
+
+            # convert from BGR to HSV
+            # format.
+            hsv = cv.cvtColor(frame1, cv.COLOR_BGR2HSV)
+
+            dst = cv.calcBackProject([hsv],
+                                     [0],
+                                     roi_hist,
+                                     [0, 180], 1)
+
+            # apply Camshift to get the
+            # new location
+            ret2, track_window = cv.CamShift(dst,
+                                             track_window,
+                                             term_crit)
+
+            # Draw it on image
+            pts = cv.boxPoints(ret2)
+
+            # convert from floating
+            # to integer
+            pts = np.int0(pts)
+
+            # Draw Tracking window on the
+            # video frame.
+            Result = cv.polylines(frame,
+                                  [pts],
+                                  True,
+                                  (0, 255, 255),
+                                  2)
+
+            cv.imshow('Camshift', Result)
+
+            # set ESC key as the
+            # exit button.
+            k = cv.waitKey(30) & 0xff
+
+            if k == 27:
+                break
+
+        # Release the cap object
+        cap.release()
+
+        # close all opened windows
+        cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+
+    Opticalflow('videoplayback.mp4', 0)
+    Camshift('videoplayback.mp4')
